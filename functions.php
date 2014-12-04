5eec954ae248afabd2e29f62085b9d811d790975
star99 <niumu99@gmail.com>
2014-12-04 04:21:04 +0100

    v0.1

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+define('HEADER_TEXTCOLOR', '');
+define('HEADER_IMAGE', '%s/img/misty.jpg'); // %s is theme dir uri
+define('HEADER_IMAGE_WIDTH', 960);
+define('HEADER_IMAGE_HEIGHT', 250);
+define( 'NO_HEADER_TEXT', true );
